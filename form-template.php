8621ad7ae4296c8e71a7f927c9dabd0fd0fe6e6b
radoslav-zdravkovic-bc <zdravkovicradoslav@gmail.com>
2024-05-09 22:04:57 +0200

    init

---
+++ form-template.php
@@ -0,0 +1,18 @@
+<form method="post">
+    <label for="gemini_data">Enter Data:</label>
+    <input type="text" name="rado_data" id="rado_data" required>
+    <input type="submit" value="Submit">
+    <?php wp_nonce_field( 'add_data_nonce_action', 'add_data_nonce_field' ); ?>
+</form>
+
+<?php
+
+if ( isset( $_POST['rado_data'] ) && wp_verify_nonce( $_POST[ 'add_data_nonce_field' ], 'add_data_nonce_action' ) )
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'simple_db_table';
+    $data = sanitize_text_field( $_POST[ 'rado_data' ] );
+    $wpdb->insert( $table_name, array( 'data' => $data ) );
+    
+    echo 'Data submitted successfully!';
+}
---
+++ wp-rado-simple-db-table.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: WP Rado Simple DB Table
+Plugin URI: https://github.com/RadoZ-dev/wp-rado-simple-db-table
+Description: Creates a database table and shortcode with a simple form to add data to the table.
+Version: 0.1.0
+Author: Radoslav Zdravkovic
+Author URI: https://github.com/RadoZ-dev
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+function wprado_create_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'simple_db_table';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        data varchar(255) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+register_activation_hook( __FILE__, 'wprado_create_table' );
+
+function wprado_form_shortcode() {
+    ob_start();
+    include 'form-template.php';
+    return ob_get_clean();
+}
+
+add_shortcode( 'wprado_form', 'wprado_form_shortcode' );
